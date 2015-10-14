4a083759ce1be2b5e887b12901075b18061d084e
phyer@sina.com <phyer@sina.com>
2015-10-15 01:13:37 +0800

    gulp css can be run, include less and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 var autoprefix = require('gulp-autoprefixer');
 
 gulp.task('css', function(){
-        gulp.src('assets/app.less')
+        gulp.src('assets/*.less')
                 .pipe(less())
                 .pipe(autoprefix('last 2 version', 'ie 8', 'ie 9'))
                 .pipe(gulp.dest('build/css'));
